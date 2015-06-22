787d67fa3651df06c1aaaad262de96d3f153b6d8
Alexander Zeier <alexander.zeier@stud.h-da.de>
2015-06-22 17:56:12 +0200

    Wischgeste für Masterview nur am linken Rand möglich

--- FireflyApp/FireflyApp/SplitViewController.swift
+++ FireflyApp/FireflyApp/SplitViewController.swift
@@ -32,5 +32,15 @@ class SplitViewController: UISplitViewController {
         masterControlView.delegate = mapView
     }
    
-    
+    override func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent) {
+        let touch = touches.first as! UITouch
+        let location = touch.locationInView(self.view)
+        
+        // Just allow the master view to appear if swipe gesture was performed at the left edge of the screen
+        if (location.x < 100) {
+            self.presentsWithGesture = true
+        } else {
+            self.presentsWithGesture = false
+        }
+    }
 }
